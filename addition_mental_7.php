e246f2a6080c1f3f75dd0137dcdf27464ee3792f
vcoquinot <vcoquinot@gmail.com>
2019-08-28 09:52:29 +0200

    added span, fa icon + bold

--- addition_mental_7.php
+++ addition_mental_7.php
@@ -8,13 +8,14 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="Site de révisions pour les élèves de CM1">
 
-      <title>+8</title>      
+      <title >+7</title>      
       <link href="style1.css"  type="text/css" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
 <!-- Bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -49,8 +50,11 @@
             <div class="col-lg-2"></div>
             <div class="col-lg-5">
               <!-- premier chiffre aléatoire-->
-              <?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber);?> + 
-              <?php echo $secondNumber;?> =
+              <span class="col-lg-1">
+              <b><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber);?></b></span>
+              <i class="fa fa-plus-circle" style="color:#FF502F"></i> 
+              <span class="col-lg-1"><b><?php echo $secondNumber;?></b></span>
+              <span class="col-lg-1"><b>=</b></span>
               <!-- transmission des données-->
               <input class="col-lg-3" name="resultatUn" type="text" placeholder="total"></input>
               <input class="col-lg-2" name="resultatCorrectUn" type="hidden"
@@ -58,22 +62,27 @@
               ?>">
             </div>
             <!--Calcul N° deux-->
-            <div class="col-lg-5"><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber) ;?> 
-            + 
-            <?php echo $secondNumber;?> = <input class="col-lg-3" name="resultatDeux" type="text" placeholder="total"></input>
+            <div class="col-lg-5">
+              <span class="col-lg-1"><b><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber) ;?></b></span>
+            <i class="fa fa-plus-circle" style="color:#FF502F"></i>
+            <span class="col-lg-1"><b><?php echo $secondNumber;?></b></span>
+            <span class="col-lg-1"><b>=</b></span>
+            <input class="col-lg-3" name="resultatDeux" type="text" placeholder="total"></input>
             
             <input class="col-lg-2" type="hidden"
             name="resultatCorrectDeux" value="<?php echo $resultatCorrectDeux= addition($randFirstNumber, $secondNumber); 
             ?>">
             </div>
           </div>
-          <!--Calcul N° quatre-->
+          <!--Calcul N° trois-->
           <div class="row">            
             <div class="col-lg-2"></div>
             <div class="col-lg-5">
               <!-- premier chiffre aléatoire-->
-              <?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber);?> + 
-              <?php echo $secondNumber;?> =
+              <span class="col-lg-1"><b><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber);?></b></span>
+              <i class="fa fa-plus-circle" style="color:#FF502F"></i> 
+              <span class="col-lg-1"><b><?php echo $secondNumber;?></b></span>
+              <span class="col-lg-1"><b>=</b></span>
               <!-- transmission des données-->
               <input class="col-lg-3" name="resultatTrois" type="text" placeholder="total"></input>
               <input class="col-lg-2" name="resultatCorrectTrois" type="hidden"
@@ -81,9 +90,12 @@
               ?>">
             </div>
             <!--Calcul N° quatre-->
-            <div class="col-lg-5"><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber) ;?> 
-            + 
-            <?php echo $secondNumber;?> = <input class="col-lg-3" name="resultatQuatre" type="text" placeholder="total"></input>
+            <div class="col-lg-5">
+              <span class="col-lg-1"><b><?php $randFirstNumber = randCountNumberWithTwoFigures($firstNumber) ;?></b></span> 
+            <i class="fa fa-plus-circle" style="color:#FF502F"></i> 
+            <span class="col-lg-1"><b><?php echo $secondNumber;?></b></span>
+            <span class="col-lg-1"><b>=</b></span>
+            <input class="col-lg-3" name="resultatQuatre" type="text" placeholder="total"></input>
             
             <input class="col-lg-2" type="hidden"
             name="resultatCorrectQuatre" value="<?php echo $resultatCorrectQuatre= addition($randFirstNumber, $secondNumber); 
