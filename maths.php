15b96e835d99f2d8444b8950174136b9f8bff5c8
vcoquinot <vcoquinot@gmail.com>
2019-09-02 10:29:38 +0200

    update size img + layout without buttons

--- maths.php
+++ maths.php
@@ -86,7 +86,7 @@
           <a href=fraction.php><img width="140" height="140" src="assets/img/fraction.png" preserveAspectRatio="xMidYMid slice" alt="fractions"><rect width="100%" height="100%"/></a>        
         </div>
         <div class="row justify-content-center col-lg-4">
-          <a href="probleme.php"><img width="140" height="140" src="assets/img/english-language.png" alt="problèmes"preserveAspectRatio="xMidYMid slice" alt="anglais"><rect width="100%" height="100%"/></a>
+          <a href="probleme.php"><img width="140" height="140" src="assets/img/puzzle.png" alt="problèmes"preserveAspectRatio="xMidYMid slice" alt="anglais"><rect width="100%" height="100%"/></a>
         </div>
         <div class="row justify-content-center col-lg-4">
           <img width="140" height="140" src="assets/img/chemistry.png" alt="sciences"preserveAspectRatio="xMidYMid slice"><rect width="100%" height="100%"/>
