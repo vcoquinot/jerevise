11dd514dd19c535f815c3c4c5c0ad73484ae442d
vcoquinot <vcoquinot@gmail.com>
2019-09-05 10:55:39 +0200

    list item bold

--- conjugaison.php
+++ conjugaison.php
@@ -50,41 +50,41 @@
 
       <div class="row justify-content-center col-lg-12">
         <div class="row justify-content-center col-lg-4">
-          <img width="140" height="140" src="assets/img/present.png" preserveAspectRatio="xMidYMid slice" alt="present">      
+          <img width="140" height="140" src="assets/img/present.png" preserveAspectRatio="xMidYMid" alt="present">      
         </div>
         <div class="row justify-content-center col-lg-4">
-          <img width="140" height="140" src="assets/img/wall-clock.png" preserveAspectRatio="xMidYMid slice" alt="futur">      
+          <img width="140" height="140" src="assets/img/wall-clock.png" preserveAspectRatio="xMidYMid" alt="futur">      
         </div>
         <div class="row justify-content-center col-lg-4">
-          <img width="140" height="140" src="assets/img/history.png" preserveAspectRatio="xMidYMid slice" alt="imparfait">
+          <img width="140" height="140" src="assets/img/history.png" preserveAspectRatio="xMidYMid" alt="imparfait">
       </div>
       <br/>
 
       <div class="row justify-content-center col-lg-12">
         <div class="row justify-content-center col-lg-4">
           <div class="row justify-content-center list-group">
-          <a href="additions_mentales.php" class="list-group-item list-group-item-action">1er groupe</a></a>
+          <b><a href="additions_mentales.php" class="list-group-item list-group-item-action">1er groupe</a></a>
           <a href="addition_mental_10" class="list-group-item list-group-item-action">-yer</a>
           <a href="addition_mental_100" class="list-group-item list-group-item-action">-re</a>
           <a href="addition_mental_1000" class="list-group-item list-group-item-action">être et avoir</a>
-          <a href="addition_mental_1000" class="list-group-item list-group-item-action">on mélange tout !</a>
+          <a href="addition_mental_1000" class="list-group-item list-group-item-action">on mélange tout !</a></b>
           </div>     
         </div>
         <div class="row justify-content-center col-lg-4">
           <div class="row justify-content-center list-group">
-          <a href="futur_1.php" class="list-group-item list-group-item-action">verbes en -ER et -IR</a></a>
+          <b><a href="futur_1.php" class="list-group-item list-group-item-action">verbes en -ER et -IR</a></a>
           <a href="futur_verbe_re.php" class="list-group-item list-group-item-action">verbes en -RE</a>
           <a href="futur_verbe_yer.php" class="list-group-item list-group-item-action">verbes en -YER</a>
           <a href="futur_auxiliaires.php" class="list-group-item list-group-item-action">être et avoir</a>
           <a href="futur_irregulier.php" class="list-group-item list-group-item-action">verbes bizarres</a>
-          <a href="futur.php" class="list-group-item list-group-item-action">un petit mélange de tout !</a>
+          <a href="futur.php" class="list-group-item list-group-item-action">un petit mélange de tout !</a></b>
         </div>     
         </div>
         <div class="row justify-content-center col-lg-4">
           <div class="list-group">
-          <a href="multiplications_mentales_unité.php" class="list-group-item list-group-item-action">* 2 à  9</a></a>
+          <b><a href="multiplications_mentales_unité.php" class="list-group-item list-group-item-action">* 2 à  9</a></a>
           <a href="multiplications_mentales_10" class="list-group-item list-group-item-action">* 10</a>
-          <a href="multiplications_mentales_100" class="list-group-item list-group-item-action">* 100</a>
+          <a href="multiplications_mentales_100" class="list-group-item list-group-item-action">* 100</a></b>
           </div>    
         </div>
       </div>
