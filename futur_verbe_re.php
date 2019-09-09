f968c93e2113af943d1c67e9791b185994f72cd2
vcoquinot <vcoquinot@gmail.com>
2019-09-09 08:40:13 +0200

    indent

--- futur_verbe_re.php
+++ futur_verbe_re.php
@@ -17,39 +17,40 @@
       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
-<!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+      <![endif]-->
   </head>
 
 
   <body>
   <?php
+
   //COMPTEUR SCORE      
   initialisationScore();
   ?>
 
-  <section id="futur_premier_groupe">
-    <div class="row justify-content-center">
-      <h2>Le futur des verbes en -RE comme prendre, peindre,...</h2>
-    </div>
-    <div class="row justify-content-center">
-      <img class="img-fluid" src="assets/img/futur_dre.png" alt="garçon">
-    </div>
-  </section>
+    <section id="futur_premier_groupe">
+      <div class="row justify-content-center">
+        <h2>Le futur des verbes en -RE comme prendre, peindre,...</h2>
+      </div>
+      <div class="row justify-content-center">
+        <img class="img-fluid" src="assets/img/futur_dre.png" alt="garçon">
+      </div>
+    </section>
     
     <!-- affichage des questions + formulaire réponse-->
     <section class="questionnaire">      
       <div class="row justify-content-center">
-        <h2 style =" color:#007065; margin-bottom:20px;">C'est parti !</h2>
+        <h2 style =" color:#007065; margin-bottom:20px;">Entraîne-toi !</h2>
       </div>
       <div class="container-fluid">
         <div class="row justify-content-center">
