7d77f3cf91def075e4df5000046ddf806fc5cd94
vcoquinot <vcoquinot@gmail.com>
2019-09-11 10:29:19 +0200

    update

--- pose.php
+++ pose.php
@@ -2,81 +2,60 @@
 <?php include("fonction.php");?>
 <?php include("fonctions_maths.php");?>
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
+  <html lang="fr">
+    <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="Site de révisions pour les élèves de CM1">
 
-      <title>additions possée à 2 chiffres</title>      
+      <title>additions posées</title>      
       <link href="main1.css"  type="text/css" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
-<!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- Font Awesome -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-<!-- Bootstrap core CSS -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-<!-- Material Design Bootstrap -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+      <!-- Bootstrap core CSS -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Material Design Bootstrap -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+      <![endif]-->
+    </head>
 
-  <body>
-   
-<?php 
-
-calculerScore(); 
-calculerNombreDeQuestionsPosees();
-
-  //****************************
-    //QUESTIONS
-    //****************************
-    //LIMITATION À 5 OPÉRATIONS OU RETOUR ACCUEIL 
-    if($_SESSION['numeroQuestion'] >5){
-      $_SESSION['numeroQuestion'] =0;
-      $_SESSION['score'] =0;
-      redirection();
-    }else{
-      ?>
+    <body>
+     
+    <?php 
+    //COMPTEUR SCORE      
+    initialisationScore();
+    ?>
       <section>
         <div class="row justify-content-center">
-          <img class="img-fluid" src="assets/img/maths_2_chiffres.png" alt="addition à deux chiffres">
+          <img width="400" preserveAspectRatio="xMidYMid slice" src="assets/img/maths_2_chiffres.png" alt="addition à deux chiffres">
         </div>
       </section>
+        
       <?php
 
-    // deux nombres aléatoires à additioner        
-    $a=0;
-    $b=0;
-    $randFirstNumber = $a.rand(10,99);;
-    $randSecondNumber = $a.rand(10,99);
-                
-  ?>
-  <br/>
-  <br/>
-   
-
-    <?php
-    // deux nombres aléatoires à additioner        
-    $a=0;
-    $b=0;
-    $randFirstNumber = $a.rand(10,99);;
-    $randSecondNumber = $a.rand(10,99);
-                
-    poseAddition($randFirstNumber, $randSecondNumber);
-    }
-    ?>
-  </body>
-</html>
+      // deux nombres aléatoires à additioner        
+      $a=0;
+      $b=0;
+      $randFirstNumber = $a.rand(10,99);;
+      $randSecondNumber = $a.rand(10,99);
+      ?>
+      <br/><br/>
+      <?php              
+      poseAddition($randFirstNumber, $randSecondNumber);
+      ?>
+    </body>
+  </html>
