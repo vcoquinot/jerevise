6e28325edfe2aad5b8fe9570ef87723d84b30746
vcoquinot <vcoquinot@gmail.com>
2019-09-13 15:12:36 +0200

    test ok

--- cible_futur.php
+++ cible_futur.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<?php include "fonction.php" ?>
+<?php include ("fonction.php"); ?>
 <?php require_once("connexion_bdd.php")?>
 
 <!DOCTYPE html>
@@ -10,16 +10,21 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <meta name="description" content="Site de révisions pour les élèves de CM1">
 
-      <title>Futur</title>      
+      <title>cible futur</title>      
       <link href="style1.css"  type="text/css" rel="stylesheet">
       <script type="text/javascript" src="assets/js/javascript.js"></script>
       <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
-<!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+      <!-- Bootstrap core CSS -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Material Design Bootstrap -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.7/css/mdb.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -30,56 +35,95 @@
   </head>
 
   <body>
-  <?php     
+    <section>
+      <div class="row justify-content-center">
+        <h2>futur</h2>
+      </div>
+      <div class="row justify-content-center">
+        <img class="img-fluid" src="assets/img/futur.png" alt="conjugaison">
+      </div>
+    </section>
+
+  <?php
   //***** RÉCUPÉRATION DES DONNÉES SESSION ET FORMULAIRE *****
-  $reponseUtilisateur = $_POST["reponseFutur"];
+  $reponseUtilisateur = $_GET["reponseFutur"];
   $idQuestion= $_SESSION['id_question'];
   $numeroQuestion = $_SESSION['numeroQuestion'];
-
-  //*****LIMITATION À 10 QUESTIONS
-  if($numeroQuestion<=9){
-    //***** TRAITEMENT DE LA RÉPONSE DE L'UTILISATEUR *****
+  
+  //*****LIMITATION À 6 QUESTIONS
+  if($numeroQuestion<=5){
+      //***** TRAITEMENT DE LA RÉPONSE DE L'UTILISATEUR *****
     //recherche de la réponse associée à la question dans la BDD
     $reponseFutur = $bdd->query("SELECT intitule_reponse 
       FROM question,reponse
       WHERE question.id_question = $idQuestion AND reponse.id_reponse = $idQuestion");
-    $donnees = $reponseFutur->fetch();
+    $donneesReponseFutur = $reponseFutur->fetch();
 
     //Comparaison réponse de l'utilisateur et réponse correcte
     $isCorrect=false;
-    $_SESSION['reponseCorrecte'] = $donnees['intitule_reponse'];   
+    $reponseCorrecte = $donneesReponseFutur['intitule_reponse'];   
     /*$pattern = '/' . preg_quote($reponseUtilisateur) . '/';*/
     $reponseFutur->closeCursor();
 
-    
 
     //CAS 1 **** RÉPONSE CORRECTE
-    if($reponseUtilisateur != null && ($reponseUtilisateur===$_SESSION['reponseCorrecte']))
+    if($reponseUtilisateur == $reponseCorrecte)
     { 
       $isCorrect === true; 
       // + 1 point
-      $_SESSION['score']++; 
-      //question suivante
-      header( "Location: futur.php"); 
+      $_SESSION['score']++;
+      $commentaireReussite = $bdd->query("SELECT commentaire_reussite FROM reussite ORDER BY RAND() LIMIT 1");
+      // recherche d'un commentaire de réussite alléatoire   
+      $donneesReussite = $commentaireReussite->fetch();
+      $felicitation = $donneesReussite['commentaire_reussite'];
+      ?>
+      <!--affichage du commentaire-->
+      <section class="row justify-content-center">
+      <h2 class="commentaire" style="color:#FF8080"><?php echo $felicitation; ?></h2>
+    </section>
+      <?php 
 
-    }//CAS 2 **** ESPACE COMMENTAIRE RÉPONSE INCORRECTE
-      else {
- 		verifierReponseUtilisateur();
-        header( "refresh:2;url=futur.php") ?>
-              </div>
-            </div>
+      //question suivante
+      header( "refresh:2;url=futur"); 
+
+    }else
+    //CAS 2 **** ESPACE COMMENTAIRE RÉPONSE INCORRECTE
+      {
+        ?>
+        <div class="container">
+          <div class="row justify-content-center ">
+            <h2 style="color:#FF8080"><b><?php  echo "Oups, mauvaise réponse !"; ?></b></h2>
           </div>
-        </section>
+        <div class="row justify-content-center col-12">
+        <?php
+        header( "refresh:2;url=futur.php") ?>
         <?php
       }
-      
+      $_SESSION['numeroQuestion']++; 
       ?>
-     <?php
+      <?php
 
-
-     //AU DESSUS DE 10 QUESTIONS
+    //AU DESSUS DE 6 QUESTIONS
     }else{
-    	afficherScore();
+      afficherScore();
+      if($_SESSION['score'] <= ($_SESSION['numeroQuestion']-1)){
+        ?>        
+        <div class="container">
+          <div class="row justify-content-center ">
+            <h2 class="commentaire" style="color:#569ef6"><b><?php  echo "Et si tu t'entraînais encore un peu ?!"; ?></b></h2> 
+          </div>
+        </div>
+      <?php
+      }
+      reinitialiserCompteurs();
+      ?>
+      <section class="container">
+        <div class="row justify-content-center">
+          <a href="futur.php"><button type="button" class="btn">Retenter</button></a>
+          <a href="accueil.php"><button type="button" class="btn">Accueil</button></a>
+        </div>
+      </section>
+    <?php
     }      
-  ?>
+    ?>
   </body>
