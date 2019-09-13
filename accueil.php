807f55953c3aaf5b0a581bcc0fd09e07ea5b7b65
vcoquinot <vcoquinot@gmail.com>
2019-09-13 14:03:46 +0200

    added collapse menu

--- accueil.php
+++ accueil.php
@@ -24,6 +24,10 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
 
   <body>
@@ -36,47 +40,107 @@
 
     <div class="container" style="background-color:white;">
       <div class="sommaire">
-<!---- haut-->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="card lg-4 align-items-center">
-            <h2 class="row text-center">FRANCAIS</h2>
-            <img width="140" height="140" src="assets/img/folders.png" preserveAspectRatio="xMidYMid slice" alt="francais" aria-label="Placeholder: Thumbnail">
-            <rect width="100%" height="100%" fill="#55595c"/>
-            <div class="card-body">
-              <b><a href="conjugaison.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/conjug.png" preserveAspectRatio="xMidYMid slice"alt="conjugaison"> conjugaison</a>
-              <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="grammaire"> grammaire</a>
-              <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/vocab.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> vocabulaire</a></b>
-              <div class="d-flex justify-content-between align-items-center">                
+        <!---- haut-->
+        <div class="container">
+          <div class="row">
+             <div class="col-lg-4">
+                <div class="card lg-4 align-items-center">
+                <h2 class="row text-center">FRANCAIS</h2>
+                <a data-toggle="collapse" href="#collapse" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/folders.png" preserveAspectRatio="xMidYMid slice" alt="francais" aria-label="Placeholder: Thumbnail"></a>
+                <br/>
+                  <div class="collapse" id="collapse">
+                    <div class="card-body">
+                    <b><a href="conjugaison.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/conjug.png" preserveAspectRatio="xMidYMid slice"alt="conjugaison"> conjugaison</a>
+                    <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="grammaire"> grammaire</a>
+                    <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/vocab.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> vocabulaire</a></b>
+                    <div class="d-flex justify-content-between align-items-center">  
+                  </div>              
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card lg-4 align-items-center">
+          <div class="col-lg-4">
+            <div class="card lg-4 align-items-center">
             <h2 class="row text-center">MATHS</h2>
-            <img width="140" height="140" src="assets/img/math.png" preserveAspectRatio="xMidYMid slice" alt="maths" aria-label="Placeholder: Thumbnail"> 
-            <rect width="100%" height="100%" fill="#55595c"/>
-            <div class="card-body">
-              <b><a href="calcul_mental.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/think.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> calcul mental</a>
-              <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/arithmetic.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> opérations posées</a>
-              <a href="conversions.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/recycle.png" preserveAspectRatio="xMidYMid slice" alt="conversions"> conversions</a>
-              <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/fraction.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> fractions</a>
-              <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/jigsaw.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> problèmes</a></b>
-              <div class="d-flex justify-content-between align-items-center">
+            <a data-toggle="collapse" href="#collapse_math" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/math.png" preserveAspectRatio="xMidYMid slice" alt="maths" aria-label="Placeholder: Thumbnail"></a>
+            <br/>
+              <div class="collapse" id="collapse_math">
+                <div class="card-body">
+                <b><a href="calcul_mental.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/think.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> calcul mental</a>
+                <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/arithmetic.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> opérations posées</a>
+                <a href="conversions.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/recycle.png" preserveAspectRatio="xMidYMid slice" alt="conversions"> conversions</a>
+                <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/fraction.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> fractions</a>
+                <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/jigsaw.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> problèmes</a></b>
+                <div class="d-flex justify-content-between align-items-center">
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="card lg-4 align-items-center">
-            <h2 class="row text-center">ANGLAIS</h2>
-              <b><a href=en_construction.html><img width="140" height="140" src="assets/img/anglais.png" preserveAspectRatio="xMidYMid slice" alt="anglais" aria-label="Placeholder: Thumbnail"></a></b>
+          <h2 class="row text-center">ANGLAIS</h2>
+          <a data-toggle="collapse" href="#collapse_anglais" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/anglais.png" preserveAspectRatio="xMidYMid slice" alt="anglais" aria-label="Placeholder: Thumbnail"></a>
+          <br/>
+            <div class="collapse" id="collapse_anglais">
                 <div class="card-body">
-              <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/pantone.png" preserveAspectRatio="xMidYMid slice"alt="colors"> colors</a></b>
-              <div class="d-flex justify-content-between align-items-center">
-              <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> vocabulaire</a></b>
+                <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/pantone.png" preserveAspectRatio="xMidYMid slice"alt="colors"> colors</a></b>
+                <div class="d-flex justify-content-between align-items-center">
+                <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> vocabulaire</a></b>
+                <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br/><br />
+
+      <div class="container">
+        <div class="row">
+           <div class="col-lg-4">
+            <div class="card lg-4 align-items-center">
+              <h2 class="row text-center">GÉOGRAPHIE</h2>
+              <a data-toggle="collapse" href="#collapse-geo" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/globe.png" preserveAspectRatio="xMidYMid slice" alt="géographie" aria-label="Placeholder: Thumbnail"></a>
+              <br/>
+              <div class="collapse" id="collapse-geo">
+              <div class="card-body">
+                <b><a href="conjugaison.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/conjug.png" preserveAspectRatio="xMidYMid slice"alt="conjugaison"> </a>
+                <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="grammaire"> </a>
+                <a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/vocab.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> </a></b>
+                <div class="d-flex justify-content-between align-items-center">  
+                </div>              
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card lg-4 align-items-center">
+              <h2 class="row text-center">HISTOIRE</h2>
+              <a data-toggle="collapse" href="#collapse_histoire" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/caravel.png" preserveAspectRatio="xMidYMid slice" alt="histoire" aria-label="Placeholder: Thumbnail"></a>
+              <br/>
+              <div class="collapse" id="collapse_histoire">
+              <div class="card-body">
+                <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/dagger.png" preserveAspectRatio="xMidYMid slice" alt="renaissance"> Moyen-âge</a>
+                <a href="renaissance.php" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/caravel.png" preserveAspectRatio="xMidYMid slice" alt="calcul mental"> Renaissance</a>               
+                </b>
+                <div class="d-flex justify-content-between align-items-center">
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card lg-4 align-items-center">
+              <h2 class="row text-center">SCIENCES</h2>
+              <a data-toggle="collapse" href="#collapse_sciences" role="button" aria-expanded="false" aria-controls="collapse"><img width="140" height="140" src="assets/img/chemistry.png" preserveAspectRatio="xMidYMid slice" alt="sciences" aria-label="Placeholder: Thumbnail"></a>
+              <br/>
+                <div class="collapse" id="collapse_sciences">
+                  <div class="card-body">
+                  <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/pantone.png" preserveAspectRatio="xMidYMid slice"alt="colors"> </a></b>
+                  <div class="d-flex justify-content-between align-items-center">
+                  <b><a href="en_construction.html" class="list-group-item list-group-item-action"><img width="40" height="40" src="assets/img/grammaire.png" preserveAspectRatio="xMidYMid slice" alt="vocabulaire"> </a></b>
+                </div>
               </div>
             </div>
           </div>
