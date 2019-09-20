9f3c1e96691d5e28b4640270852a5d6532719e68
vcoquinot <vcoquinot@gmail.com>
2019-09-20 09:04:18 +0200

    fonction

--- cible_calcul_mental.php
+++ cible_calcul_mental.php
@@ -5,20 +5,20 @@
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <meta name="description" content="Site de révisions pour les élèves de CM1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Site de révisions pour les élèves de CM1">
 
-      <title>cible calcul mental</title>      
-      <link href="main1.css"  type="text/css" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
+    <title>cible calcul mental</title>      
+    <link href="main1.css"  type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,500" rel="stylesheet">
 
-<!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -85,7 +85,20 @@
             ?>
             <h2 class="commentaire" style="color:#FF8080"><?php  echo "Entraîne-toi encore un peu pour obtenir un max de points !"; ?></h2>
           <?php
-          }  
+          }
+
+          //RÉINITIALISATION DES DONNÉES
+          $score = 0; 
+          $resultatUn= 0;
+          $resultatCorrectUn= 0;
+          $resultatDeux= 0;
+          $resultatCorrectDeux= 0;
+          $resultattrois= 0;
+          $resultatCorrectTrois= 0;
+          $resultatQuatre= 0;
+          $resultatCorrectQuatre= 0;
+          
+          //REDIRECTION CALCUL MENTAL OU ACCUEIL
           redirection();
           ?>  
         </div>
